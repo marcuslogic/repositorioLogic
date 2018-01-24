32af0dee1b5198c763d28472aecbcfd5b60a22b2
Marcus Sousa <marcus.sousa@logicsp.com.br>
2018-01-24 00:14:52 -0200

    alteracoes

--- Basico/Fonte/treinamentoservice-web/src/main/java/br/com/logic/treinamentoservices/interfaces/ILancamentoMensalResource.java
+++ Basico/Fonte/treinamentoservice-web/src/main/java/br/com/logic/treinamentoservices/interfaces/ILancamentoMensalResource.java
@@ -5,19 +5,20 @@ import br.com.logic.dao.model.LancamentoMensalModel;
 import javax.ejb.Local;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
+import java.util.List;
 
 @Local
 public interface ILancamentoMensalResource {
 
-    public Response incluirLancamentoMensal(LancamentoMensalModel saida) throws Exception;
+//    public Response addLancamentoMensal(LancamentoMensalModel saida) throws Exception;
+//
+//    public Response alterarLancamentoMensal(LancamentoMensalModel saida) throws Exception;
+//
+//    public Response excluirLancamentoMensal(LancamentoMensalModel saida) throws Exception;
+//
+//    public Response pesquisarLancamentoPorPeriodo(LancamentoMensalModel saida) throws Exception;
 
-    public Response alterarLancamentoMensal(LancamentoMensalModel saida) throws Exception;
+    public List<LancamentoMensalModel> pesquisarLancamentoPorDescricao(String descricao, Integer tipo) throws Exception;
 
-    public Response excluirLancamentoMensal(LancamentoMensalModel saida) throws Exception;
-
-    public Response pesquisarLancamentoPorPeriodo(LancamentoMensalModel saida) throws Exception;
-
-    public Response pesquisarLancamentoPorDescricao(LancamentoMensalModel saida) throws Exception;
-
-    public Response pesquisarLancamentoPorTipo(LancamentoMensalModel saida) throws Exception;
+//    public List<LancamentoMensalModel> pesquisarLancamentoPorTipo(Integer tipo) throws Exception;
 }
--- Basico/Fonte/treinamentoservice-web/src/main/java/br/com/logic/treinamentoservices/resources/LancamentoMensalResource.java
+++ Basico/Fonte/treinamentoservice-web/src/main/java/br/com/logic/treinamentoservices/resources/LancamentoMensalResource.java
@@ -6,135 +6,118 @@ import br.com.logic.treinamentoservices.interfaces.ILancamentoMensalResource;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.util.ArrayList;
+import java.util.List;
 
 @Stateless
 @Path("/")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class LancamentoMensalResource implements ILancamentoMensalResource {
 
     @Inject
     ILancamentoMensalService lancamentoMensalService;
 
-    @POST
-    @Path("incluirLancamentoMensal")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
+    //    @POST
+//    @Override
+//    public Response addLancamentoMensal(LancamentoMensalModel lancamentoMensal) throws Exception {
+//
+//        Response.ResponseBuilder retorno;
+//
+//        try {
+//            LancamentoMensalModel novoLancamento = lancamentoMensalService.incluirLancamentoMensal(lancamentoMensal);
+//            retorno = Response.ok(novoLancamento);
+//        } catch (Exception e) {
+//            retorno = Response.serverError().entity(e.getLocalizedMessage());
+//        }
+//
+//        return retorno.build();
+//    }
+//
+//    @POST
+//    @Path("alterarLancamentoMensal")
+//    @Override
+//    public Response alterarLancamentoMensal(LancamentoMensalModel lancamentoMensal) throws Exception {
+//
+//        Response.ResponseBuilder retorno = null;
+//
+//        try {
+//            LancamentoMensalModel lancamentoAlterado = lancamentoMensalService.alterarLancamentoMensal(lancamentoMensal);
+//            retorno = Response.ok(lancamentoAlterado);
+//        } catch (Exception e) {
+//            retorno = Response.serverError().entity(e.getLocalizedMessage());
+//        }
+//
+//        return retorno.build();
+//    }
+//
+//    @POST
+//    @Path("excluirLancamentoMensal")
+//    @Override
+//    public Response excluirLancamentoMensal(LancamentoMensalModel lancamentoMensal) throws Exception {
+//
+//        Response.ResponseBuilder retorno = null;
+//
+//        try {
+//            if(lancamentoMensalService.deletarLancamentoMensal(lancamentoMensal)) {
+//                retorno = Response.ok("Lancamento excluído.");
+//            } else {
+//                retorno = Response.ok("Não foi encontrado lacamento com ID:" + lancamentoMensal.getIdLancamento());
+//            }
+//        } catch (Exception e) {
+//            retorno = Response.serverError().entity(e.getLocalizedMessage());
+//        }
+//
+//        return retorno.build();
+//    }
+//
+//    @POST
+//    @Path("pesquisarLancamentoPorPeriodo")
+//    @Override
+//    public Response pesquisarLancamentoPorPeriodo(LancamentoMensalModel saida) throws Exception {
+//
+//        Response.ResponseBuilder retorno = null;
+//
+//        try {
+//            retorno = Response.ok(lancamentoMensalService.pesquisarLancamentosPorPeriodo(saida));
+//        } catch (Exception e) {
+//            retorno = Response.serverError().entity(e.getLocalizedMessage());
+//        }
+//
+//        return retorno.build();
+//
+//    }
+//
+    @GET
     @Override
-    public Response incluirLancamentoMensal(LancamentoMensalModel lancamentoMensal) throws Exception {
-
-        Response.ResponseBuilder retorno = null;
+    public List<LancamentoMensalModel> pesquisarLancamentos(@QueryParam("campo") String campo, @QueryParam("valor") String valor) throws Exception {
 
         try {
-            LancamentoMensalModel novoLancamento = lancamentoMensalService.incluirLancamentoMensal(lancamentoMensal);
-            retorno = Response.ok(novoLancamento);
-        } catch (Exception e) {
-            retorno = Response.serverError().entity(e.getLocalizedMessage());
-        }
-
-        return retorno.build();
-    }
-
-    @POST
-    @Path("alterarLancamentoMensal")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Override
-    public Response alterarLancamentoMensal(LancamentoMensalModel lancamentoMensal) throws Exception {
-
-        Response.ResponseBuilder retorno = null;
-
-        try {
-            LancamentoMensalModel lancamentoAlterado = lancamentoMensalService.alterarLancamentoMensal(lancamentoMensal);
-            retorno = Response.ok(lancamentoAlterado);
-        } catch (Exception e) {
-            retorno = Response.serverError().entity(e.getLocalizedMessage());
-        }
-
-        return retorno.build();
-    }
-
-    @POST
-    @Path("excluirLancamentoMensal")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Override
-    public Response excluirLancamentoMensal(LancamentoMensalModel lancamentoMensal) throws Exception {
-
-        Response.ResponseBuilder retorno = null;
-
-        try {
-            if(lancamentoMensalService.deletarLancamentoMensal(lancamentoMensal)) {
-                retorno = Response.ok("Lancamento excluído.");
+            if(descricao == null) {
+                saida.setIdTipoLancamento(tipo);
+                return lancamentoMensalService.pesqusiarLancamentosPorTipo(saida);
             } else {
-                retorno = Response.ok("Não foi encontrado lacamento com ID:" + lancamentoMensal.getIdLancamento());
+                saida.setDescricaoLancamento(descricao);
+                return lancamentoMensalService.pesquisarLancamentosPorDescricao(saida);
             }
         } catch (Exception e) {
-            retorno = Response.serverError().entity(e.getLocalizedMessage());
+            throw new Exception(e);
         }
-
-        return retorno.build();
     }
 
-    @POST
-    @Path("pesquisarLancamentoPorPeriodo")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Override
-    public Response pesquisarLancamentoPorPeriodo(LancamentoMensalModel saida) throws Exception {
-
-        Response.ResponseBuilder retorno = null;
-
-        try {
-            retorno = Response.ok(lancamentoMensalService.pesquisarLancamentosPorPeriodo(saida));
-        } catch (Exception e) {
-            retorno = Response.serverError().entity(e.getLocalizedMessage());
-        }
-
-        return retorno.build();
-
-    }
-
-    @POST
-    @Path("pesquisarLancamentoPorDescricao")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Override
-    public Response pesquisarLancamentoPorDescricao(LancamentoMensalModel saida) throws Exception {
-
-        Response.ResponseBuilder retorno = null;
-
-        try {
-            retorno = Response.ok(lancamentoMensalService.pesquisarLancamentosPorDescricao(saida));
-        } catch (Exception e) {
-            retorno = Response.serverError().entity(e.getLocalizedMessage());
-        }
-
-        return retorno.build();
-
-    }
-
-    @POST
-    @Path("pesquisarLancamentoPorTipo")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Override
-    public Response pesquisarLancamentoPorTipo(LancamentoMensalModel saida) throws Exception {
-
-        Response.ResponseBuilder retorno = null;
-
-        try {
-            retorno = Response.ok(lancamentoMensalService.pesqusiarLancamentosPorTipo(saida));
-        } catch (Exception e) {
-            retorno = Response.serverError().entity(e.getLocalizedMessage());
-        }
-
-        return retorno.build();
-
-    }
+//    @GET
+//    @Override
+//    public List<LancamentoMensalModel> pesquisarLancamentoPorTipo(Integer tipo) throws Exception {
+//
+//        LancamentoMensalModel saida = new LancamentoMensalModel();
+//        saida.setIdTipoLancamento(tipo);
+//
+//        try {
+//            return lancamentoMensalService.pesqusiarLancamentosPorTipo(saida);
+//        } catch (Exception e) {
+//            throw new Exception(e);
+//        }
+//    }
 }
